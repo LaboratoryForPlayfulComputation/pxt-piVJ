37a1c3a7fe3c836a5d44030788326ace10dbb0a0
shadowfax6894 <anniekelly6894@gmail.com>
2018-09-25 17:44:16 -0600

    added more functions

--- vj.ts
+++ vj.ts
@@ -46,5 +46,49 @@ namespace vj {
         serial.writeLine("mute 0");
     }    
   
+    /**
+     * Set youtube video by id
+     */
+    //% weight=90
+    //% blockId=youtube_setvideo block="set video by id %id" blockGap=8
+    export function setVideoById(id: string): void {
+        let command = "id " + id;
+        serial.writeLine(command);
+    }    
+
+    /**
+     * Set playback rate
+     * @param speed the playback rate of the video between ``0`` and ``200``, eg: 100
+     */
+    //% weight=90
+    //% blockId=youtube_setrate block="set playback rate %speed| %" blockGap=8
+    //% speed.min=0 speed.max=200
+    export function setPlaybackRate(speed: number): void {
+        let command = "scaledspeed " + speed;
+        serial.writeLine(command);
+    }  
+
+    /**
+     * Seek to
+     */
+    //% weight=90
+    //% blockId=youtube_seekto block="seek to %hour| : %minute| : %second" blockGap=8
+    export function seekTo(hour: number, minute: number, second: number): void {
+        let hoursToSeconds = hour * 60 * 60;
+        let minutesToSeconds = minute * 60;
+        let time = hour + minute + second; 
+        let command = "seek " + time;
+        serial.writeLine(command);
+    }  
+
+    /**
+     * Set volume
+     */
+    //% weight=90
+    //% blockId=youtube_setvolume block="set volume %volume" blockGap=8
+    export function setVolume(volume: number): void {
+        let command = "volume " + volume;
+        serial.writeLine(command);
+    }  
 
 }
